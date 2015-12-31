783a01e8497a7b5353eb63b43a00a1c482ff97e2
Pioneer11X <sravan@mysmartprice.com>
2015-12-31 19:28:01 +0530

    At last. A working Copy. Need to add the images and stuff though

--- testApp/ViewController.swift
+++ testApp/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var heroImageView: UIImageView!
+    @IBOutlet var heroTextView: UITextView!
+    @IBOutlet var inputTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        inputTextField.delegate = self
+        print("Loaded")
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?){
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool{
+        
+        let heroName = textField.text
+        print(heroName)
+        let newHeroClass = heroData(heroName: heroName)
+        heroTextView.text = newHeroClass.heroBio
+        print(newHeroClass.heroBio)
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    @IBAction func Button(sender: UISegmentedControl) {
+        
+        
+        
+    }
 
 }
 
---
+++ testApp/appModel.swift
@@ -0,0 +1,35 @@
+//
+//  appModel.swift
+//  testApp
+//
+//  Created by Sravan on 31/12/15.
+//  Copyright © 2015 pioneer11x. All rights reserved.
+//
+
+import Foundation
+
+class heroData{
+    
+    var heroName = "dummy";
+    var heroImageName = "Default";
+    var heroBio = "Select one of the options above"
+    var heroStats = "Primary Attribute Displayed here"
+    var heroAbilities: [String] = []
+    
+    init(heroName: String?){
+        
+        if heroName == nil {
+            return
+        }
+        
+        if let bio = bioData[heroName!]{
+            
+            self.heroBio = bio
+            self.heroImageName = imageData[heroName!]!
+            return
+            
+        }
+
+    }
+    
+}
